5a20661899bd5e3b06ff8029328837a2e556f56a
Annesha Naskar <annesha947n@gmail.com>
2023-11-27 17:56:49 +0530

    third commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 
 const Contact = () => {
+  const [data, setData] = useState(
+    {
+      name: "",
+      email: "",
+      msg: "",
+    }
+  );
+  console.log(data);
   return (
     <>
       <Header />
-      <h1>Welcome to Contact Us pages</h1>
+      <section id="contact-us">
+        <h1 className="heading">Contact Us</h1>
+        <div className="container">
+          <form>
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" required
+              value={data.name}
+              onChange={(e) => setData({ ...data, name: e.target.value })} />
+
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email" required
+              value={data.email}
+              onChange={(e) => setData({ ...data, email: e.target.value })} />
+
+            <label htmlFor="message">Message: </label>
+            <input type="text" id="message" required
+              value={data.msg}
+              onChange={(e) => setData({ ...data, msg: e.target.value })} />
+
+            <div className="btns">
+              <input type="submit" />
+              <input type="reset" />
+            </div>
+          </form>
+        </div>
+      </section>
     </>
   )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,38 +7,60 @@ const Products = () => {
             <Header />
             <section className="products" id="product">
                 <div className="container">
-                    <h1>Our Products</h1><br /> <hr />
+                    <h1>Our Products</h1><br /><hr />
                     <div className="row">
+                        <div className="type"><h1>CATEGORIES</h1></div>
                         <div className="pcol">
                             <h2 className="heading"> ESSENTIALS </h2>
                             <img src={require("../images/toothbrush.jpg")} alt="" />
                             <div className="pdesc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, distinctio a ipsa, quas aliquid dolores veniam quo sed unde eveniet enim pariatur delectus deleniti quaerat atque consectetur nostrum labore asperiores?</p>
+                                <p>Finely crafted just for you!</p>
                             </div>
-                            <button className="btn">SHOP NOW</button>
+                            <button className="btn">View More</button>
                         </div>
                         <div className="pcol">
                             <h2 className="heading"> DECOR </h2>
                             <img src={require("../images/i7.jpg")} alt="" />
                             <div className="pdesc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, distinctio a ipsa, quas aliquid dolores veniam quo sed unde eveniet enim pariatur delectus deleniti quaerat atque consectetur nostrum labore asperiores?</p>
+                                <p>Finely crafted just for you!</p>
                             </div>
-                            <button className="btn">SHOP NOW</button>
+                            <button className="btn">View More</button>
                         </div>
                         <div className="pcol">
                             <h2 className="heading"> DAILY USE </h2>
                             <img src={require("../images/utensils.jpg")} alt="" />
                             <div className="pdesc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, distinctio a ipsa, quas aliquid dolores veniam quo sed unde eveniet enim pariatur delectus deleniti quaerat atque consectetur nostrum labore asperiores?</p>
+                                <p>Finely crafted just for you!</p>
                             </div>
+                            <button className="btn">View More</button>
+                        </div>
+                        <div className="pcol">
+                            <h2 className="heading"> ALL PRODUCTS </h2>
+                            <img src={require("../images/i8.jpg")} alt="" />
+                            <div className="pdesc">
+                                <p>Finely crafted just for you!</p>
+                            </div>
+                            <button className="btn">View More</button>
+                        </div>
+                        <div className="type"><h1>BEST SELLERS</h1></div>
+                        <div className="pcol">
+                            <h2 className="heading"> VASE </h2>
+                            <img src={require("../images/vase.jpg")} alt="" />
                             <button className="btn">SHOP NOW</button>
                         </div>
                         <div className="pcol">
-                            <h2 className="heading"> VIEW ALL </h2>
-                            <img src={require("../images/i8.jpg")} alt="" />
-                            <div className="pdesc">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, distinctio a ipsa, quas aliquid dolores veniam quo sed unde eveniet enim pariatur delectus deleniti quaerat atque consectetur nostrum labore asperiores?</p>
-                            </div>
+                            <h2 className="heading"> LAMPS </h2>
+                            <img src={require("../images/Lamp.jpg")} alt="" />
+                            <button className="btn">SHOP NOW</button>
+                        </div>
+                        <div className="pcol">
+                            <h2 className="heading"> GLASS </h2>
+                            <img src={require("../images/glasses.jpg")} alt="" />
+                            <button className="btn">SHOP NOW</button>
+                        </div>
+                        <div className="pcol">
+                            <h2 className="heading"> PLANTER </h2>
+                            <img src={require("../images/Planter.jpg")} alt="" />
                             <button className="btn">SHOP NOW</button>
                         </div>
                     </div>
